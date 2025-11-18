a1bbd0128ee356edebcae1aaa8a002b1a51c7e65
Jonathan Teel <jonathan.teel.work@gmail.com>
2025-11-18 23:58:26 +0000

    began

--- mean_var_std.py
+++ mean_var_std.py
@@ -1,6 +1,11 @@
 import numpy as np
 
 def calculate(list):
+    myArr = np.array([[list[0],list[1],list[2]],[list[3],list[4],list[5]],[list[6],list[7],list[8]]])
+    calculations = {}
+    
+
+
 
 
 
